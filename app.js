4c1c9a5ba81362f9c444d6770365910497f126b0
Felipe1012 <60628168+Felipe1012@users.noreply.github.com>
2020-10-27 16:50:56 -0500

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,23 @@ app.use(cors());
 
 app.post('/stt',(req,res)=>{
 
-   res.send("hola")
+    var request = require('request');
+    var options = {
+      'method': 'POST',
+      'url': 'https://api.us-south.speech-to-text.watson.cloud.ibm.com/instances/2c86099d-5a2e-4191-a375-797ec3d1b2d6/v1/recognize',
+      'headers': {
+        'Content-Type': 'audio/flac',
+        'Authorization': 'Basic YXBpa2V5OnBGdWs2V2REaDFxckdJeFVXQXY1NXFqREFOVGQzdmlsa1AzdVZDc1ZqVUdq'
+      },
+      body: req
+
+    };
+    request(options, function (error, response) {
+      if (error) throw new Error(error);
+      console.log(response.body);
+      res.send("holaaaaaa")
+
+    });
 })
 
 app.listen(app.get('port'),()=>{
