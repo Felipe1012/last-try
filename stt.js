7e428685ac9a4ed5b9e78856c84a2fcf6056c018
Felipe1012 <60628168+Felipe1012@users.noreply.github.com>
2020-11-11 15:42:25 -0500

    Update stt.js

--- stt.js
+++ stt.js
@@ -2,32 +2,34 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan')
 const cors = require("cors");
-const fs=require('fs');
+const fs = require('fs');
 const SpeechToTextV1 = require('ibm-watson/speech-to-text/v1');
-function stt(input){
-    return new Promise(function (resolve, reject) {
+function stt(input) {
+  return new Promise(function (resolve, reject) {
     var audio = input
     var request = require('request');
     var options = {
       'method': 'POST',
-      'url': 'https://api.us-south.speech-to-text.watson.cloud.ibm.com/instances/2c86099d-5a2e-4191-a375-797ec3d1b2d6/v1/recognize',
+      'url': 'https://api.us-south.speech-to-text.watson.cloud.ibm.com/instances/2c86099d-5a2e-4191-a375-797ec3d1b2d6/v1/recognize?model=es-ES_BroadbandModel',
+
       'headers': {
         'Content-Type': 'audio/flac',
         'Authorization': 'Basic YXBpa2V5OnBGdWs2V2REaDFxckdJeFVXQXY1NXFqREFOVGQzdmlsa1AzdVZDc1ZqVUdq'
       },
-      body: audio
+      body: audio,
+
     };
-      console.log(audio)
-  
+    console.log(audio)
+
     request(options, function (error, response) {
 
       if (error) throw new Error(error);
-      console.log(response);
-      return resolve(response)
+      console.log(response.body);
+      return resolve(response.body)
     });
 
 
-      });
+  });
 }
 
-module.exports=stt;
+module.exports = stt;
