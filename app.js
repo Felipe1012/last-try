f54a710299bca7c136bb2ffe730bcef7c8636d51
Felipe1012 <60628168+Felipe1012@users.noreply.github.com>
2020-11-12 12:51:39 -0500

    v5

--- app.js
+++ app.js
@@ -16,23 +16,23 @@ app.use(express.json());
 app.use(cors());
 
 
-app.post('/stt',  (req, res)=> {
-console.log("jelo")
-var audio = req
+app.post('/stt', (req, res) => {
+  console.log("jelo")
+  var audio = req
 
   try {
     let finalJson = []
 
-     stt(audio).then((ans) => {
-       console.log("este si")
-       callNLUnderstanding(params,ans).then((respuesta)=>{
-         console.log("primero")
+    stt(audio).then((ans) => {
+      console.log("este si")
+      callNLUnderstanding(params, ans).then((respuesta) => {
+        console.log("primero")
         proDataNL(respuesta).then((finalRes) => {
           console.log("segundo")
           finalJson.push(finalRes)
-         res.send(finalRes)
+          res.send(finalRes)
         })
-       })
+      })
     })
   } catch (err) {
     res.status(500).json({ message: "No se pudo analizar el audio ingresado" });
